15a972301144bdbfb82e1515134a1c7203c27abe
Mariusz Postol <mpostol@cas.eu>
2021-09-20 22:47:00 +0200

    Move UI to separate project #18
    - renamed Program to ModelCompilerUI/EntryPoint.cs

--- ModelCompilerUI/Properties/AssemblyInfo.cs
+++ ModelCompilerUI/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ModelCompilerUI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Model Compiler User Interface")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("MPostol")]
 [assembly: AssemblyProduct("ModelCompilerUI")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
