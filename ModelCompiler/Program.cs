f9efadfe50344dead42e3702e9dad2f6d467e747
Mariusz <mpostol@cas.eu>
2021-10-09 15:24:53 +0200

    Create an abstract API for the library #31
    - added API

--- ModelCompiler/Program.cs
+++ ModelCompiler/Program.cs
@@ -1,315 +1,308 @@
-/* ========================================================================
- * Copyright (c) 2005-2021 The OPC Foundation, Inc. All rights reserved.
- *
- * OPC Foundation MIT License 1.00
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- *
- * The complete license agreement can be found here:
- * http://opcfoundation.org/License/MIT/1.00/
- * ======================================================================*/
+﻿//__________________________________________________________________________________________________
+//
+//  Copyright (C) 2021, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community at GitHub: https://github.com/mpostol/OPC-UA-OOI/discussions
+//__________________________________________________________________________________________________
 
+using ModelCompiler;
 using System;
 using System.Collections.Generic;
 using System.IO;
 
-namespace ModelCompiler
+namespace OOI.ModelCompiler
 {
-  public static class Program
+  public abstract class ModelCompilerAPI
   {
-    /// <summary>
-    /// Processes the command line arguments.
-    /// </summary>
-    public static void ProcessCommandLine(List<string> tokens)
+    private List<string> designFiles = new List<string>();
+    private string identifierFile = null;
+    private string outputDir = null;
+    private bool generateIds = false;
+    private uint startId = 1;
+    private string stackRootDir = null;
+    private string ansicRootDir = null;
+    private bool generateMultiFile = false;
+    private bool useXmlInitializers = false;
+    private string[] excludeCategories = null;
+    private bool includeDisplayNames = false;
+    private bool useAllowSubtypes = false;
+
+    private bool updateHeaders = false;
+    private string inputDirectory = ".";
+    private string filePattern = "*.xml";
+    private string specificationVersion = "";
+    private HeaderUpdateTool.LicenseType licenseType = HeaderUpdateTool.LicenseType.MITXML;
+    private bool silent = false;
+
+
+    public abstract void Execute();
+
+    public static ModelCompilerAPI Factory(List<string> tokens)
     {
-      List<string> designFiles = new List<string>();
-      string identifierFile = null;
-      string outputDir = null;
-      bool generateIds = false;
-      uint startId = 1;
-      string stackRootDir = null;
-      string ansicRootDir = null;
-      bool generateMultiFile = false;
-      bool useXmlInitializers = false;
-      string[] excludeCategories = null;
-      bool includeDisplayNames = false;
-      bool useAllowSubtypes = false;
+      ModelCompilerAPIInternal instance = new ModelCompilerAPIInternal();
+      instance.ProcessCommandLine(tokens);
+      return instance;
+    }
 
-      bool updateHeaders = false;
-      string inputDirectory = ".";
-      string filePattern = "*.xml";
-      string specificationVersion = "";
-      HeaderUpdateTool.LicenseType licenseType = HeaderUpdateTool.LicenseType.MITXML;
-      bool silent = false;
-
-      for (int ii = 1; ii < tokens.Count; ii++)
+    private class ModelCompilerAPIInternal : ModelCompilerAPI
+    {
+      public void ProcessCommandLine(List<string> tokens)
       {
-        if (tokens[ii] == "-input")
+        for (int ii = 1; ii < tokens.Count; ii++)
         {
-          if (ii >= tokens.Count - 1)
+          if (tokens[ii] == "-input")
           {
-            throw new ArgumentException("Incorrect number of parameters specified with the -input option.");
+            if (ii >= tokens.Count - 1)
+            {
+              throw new ArgumentException("Incorrect number of parameters specified with the -input option.");
+            }
+
+            inputDirectory = tokens[++ii];
+            continue;
           }
 
-          inputDirectory = tokens[++ii];
-          continue;
-        }
-
-        if (tokens[ii] == "-pattern")
-        {
-          if (ii >= tokens.Count - 1)
+          if (tokens[ii] == "-pattern")
           {
-            throw new ArgumentException("Incorrect number of parameters specified with the -pattern option.");
+            if (ii >= tokens.Count - 1)
+            {
+              throw new ArgumentException("Incorrect number of parameters specified with the -pattern option.");
+            }
+
+            filePattern = tokens[++ii];
+            continue;
           }
 
-          filePattern = tokens[++ii];
-          continue;
-        }
-
-        if (tokens[ii] == "-silent")
-        {
-          silent = true;
-          continue;
-        }
-
-        if (tokens[ii] == "-license")
-        {
-          if (ii >= tokens.Count - 1)
+          if (tokens[ii] == "-silent")
           {
-            throw new ArgumentException("Incorrect number of parameters specified with the -license option.");
+            silent = true;
+            continue;
           }
 
-          updateHeaders = true;
-          licenseType = (HeaderUpdateTool.LicenseType)Enum.Parse(typeof(HeaderUpdateTool.LicenseType), tokens[++ii]);
-          continue;
-        }
-
-        if (tokens[ii] == "-d2")
-        {
-          if (ii >= tokens.Count - 1)
+          if (tokens[ii] == "-license")
           {
-            throw new ArgumentException("Incorrect number of parameters specified with the -d2 option.");
+            if (ii >= tokens.Count - 1)
+            {
+              throw new ArgumentException("Incorrect number of parameters specified with the -license option.");
+            }
+
+            updateHeaders = true;
+            licenseType = (HeaderUpdateTool.LicenseType)Enum.Parse(typeof(HeaderUpdateTool.LicenseType), tokens[++ii]);
+            continue;
           }
 
-          designFiles.Add(tokens[++ii]);
-          continue;
-        }
-
-        if (tokens[ii] == "-d2")
-        {
-          if (ii >= tokens.Count - 1)
+          if (tokens[ii] == "-d2")
           {
-            throw new ArgumentException("Incorrect number of parameters specified with the -d2 option.");
+            if (ii >= tokens.Count - 1)
+            {
+              throw new ArgumentException("Incorrect number of parameters specified with the -d2 option.");
+            }
+
+            designFiles.Add(tokens[++ii]);
+            continue;
           }
 
-          designFiles.Add(tokens[++ii]);
-          continue;
-        }
-
-        if (tokens[ii] == "-d2")
-        {
-          if (ii >= tokens.Count - 1)
+          if (tokens[ii] == "-d2")
           {
-            throw new ArgumentException("Incorrect number of parameters specified with the -d2 option.");
+            if (ii >= tokens.Count - 1)
+            {
+              throw new ArgumentException("Incorrect number of parameters specified with the -d2 option.");
+            }
+
+            designFiles.Add(tokens[++ii]);
+            continue;
           }
 
-          designFiles.Add(tokens[++ii]);
-          continue;
-        }
-
-        if (tokens[ii] == "-c" || tokens[ii] == "-cg")
-        {
-          if (ii >= tokens.Count - 1)
+          if (tokens[ii] == "-d2")
           {
-            throw new ArgumentException("Incorrect number of parameters specified with the -c or -cg option.");
+            if (ii >= tokens.Count - 1)
+            {
+              throw new ArgumentException("Incorrect number of parameters specified with the -d2 option.");
+            }
+
+            designFiles.Add(tokens[++ii]);
+            continue;
           }
 
-          generateIds = tokens[ii] == "-cg";
-          identifierFile = tokens[++ii];
-          continue;
-        }
-
-        if (tokens[ii] == "-o")
-        {
-          if (ii >= tokens.Count - 1)
+          if (tokens[ii] == "-c" || tokens[ii] == "-cg")
           {
-            throw new ArgumentException("Incorrect number of parameters specified with the -o option.");
+            if (ii >= tokens.Count - 1)
+            {
+              throw new ArgumentException("Incorrect number of parameters specified with the -c or -cg option.");
+            }
+
+            generateIds = tokens[ii] == "-cg";
+            identifierFile = tokens[++ii];
+            continue;
           }
 
-          outputDir = tokens[++ii];
-          continue;
-        }
-
-        if (tokens[ii] == "-o2")
-        {
-          if (ii >= tokens.Count - 1)
+          if (tokens[ii] == "-o")
           {
-            throw new ArgumentException("Incorrect number of parameters specified with the -o option.");
+            if (ii >= tokens.Count - 1)
+            {
+              throw new ArgumentException("Incorrect number of parameters specified with the -o option.");
+            }
+
+            outputDir = tokens[++ii];
+            continue;
           }
 
-          outputDir = tokens[++ii];
-          generateMultiFile = true;
-          continue;
-        }
-
-        if (tokens[ii] == "-id")
-        {
-          startId = Convert.ToUInt32(tokens[++ii]);
-          continue;
-        }
-
-        if (tokens[ii] == "-version")
-        {
-          if (ii >= tokens.Count - 1)
+          if (tokens[ii] == "-o2")
           {
-            throw new ArgumentException("Incorrect number of parameters specified with the -version option.");
+            if (ii >= tokens.Count - 1)
+            {
+              throw new ArgumentException("Incorrect number of parameters specified with the -o option.");
+            }
+
+            outputDir = tokens[++ii];
+            generateMultiFile = true;
+            continue;
           }
 
-          specificationVersion = tokens[++ii];
-          continue;
-        }
-
-        if (tokens[ii] == "-ansic")
-        {
-          if (ii >= tokens.Count - 1)
+          if (tokens[ii] == "-id")
           {
-            throw new ArgumentException("Incorrect number of parameters specified with the -ansic option.");
+            startId = Convert.ToUInt32(tokens[++ii]);
+            continue;
           }
 
-          ansicRootDir = tokens[++ii];
-          continue;
-        }
-
-        if (tokens[ii] == "-useXmlInitializers")
-        {
-          useXmlInitializers = true;
-          continue;
-        }
-
-        if (tokens[ii] == "-includeDisplayNames")
-        {
-          includeDisplayNames = true;
-          continue;
-        }
-
-        if (tokens[ii] == "-stack")
-        {
-          if (ii >= tokens.Count - 1)
+          if (tokens[ii] == "-version")
           {
-            throw new ArgumentException("Incorrect number of parameters specified with the -stack option.");
+            if (ii >= tokens.Count - 1)
+            {
+              throw new ArgumentException("Incorrect number of parameters specified with the -version option.");
+            }
+
+            specificationVersion = tokens[++ii];
+            continue;
           }
 
-          stackRootDir = tokens[++ii];
-          continue;
-        }
-
-        if (tokens[ii] == "-exclude")
-        {
-          if (ii >= tokens.Count - 1)
+          if (tokens[ii] == "-ansic")
           {
-            throw new ArgumentException("Incorrect number of parameters specified with the -exclude option.");
+            if (ii >= tokens.Count - 1)
+            {
+              throw new ArgumentException("Incorrect number of parameters specified with the -ansic option.");
+            }
+
+            ansicRootDir = tokens[++ii];
+            continue;
           }
 
-          excludeCategories = tokens[++ii].Split(',', '+');
-          continue;
-        }
+          if (tokens[ii] == "-useXmlInitializers")
+          {
+            useXmlInitializers = true;
+            continue;
+          }
 
-        if (tokens[ii] == "-useAllowSubtypes")
-        {
-          useAllowSubtypes = true;
-          continue;
+          if (tokens[ii] == "-includeDisplayNames")
+          {
+            includeDisplayNames = true;
+            continue;
+          }
+
+          if (tokens[ii] == "-stack")
+          {
+            if (ii >= tokens.Count - 1)
+            {
+              throw new ArgumentException("Incorrect number of parameters specified with the -stack option.");
+            }
+
+            stackRootDir = tokens[++ii];
+            continue;
+          }
+
+          if (tokens[ii] == "-exclude")
+          {
+            if (ii >= tokens.Count - 1)
+            {
+              throw new ArgumentException("Incorrect number of parameters specified with the -exclude option.");
+            }
+
+            excludeCategories = tokens[++ii].Split(',', '+');
+            continue;
+          }
+
+          if (tokens[ii] == "-useAllowSubtypes")
+          {
+            useAllowSubtypes = true;
+            continue;
+          }
         }
       }
 
-      if (updateHeaders)
+      public override void Execute()
       {
-        HeaderUpdateTool.ProcessDirectory(inputDirectory, filePattern, licenseType, silent);
-        return;
-      }
+        if (updateHeaders)
+        {
+          HeaderUpdateTool.ProcessDirectory(inputDirectory, filePattern, licenseType, silent);
+          return;
+        }
 
-      ModelGenerator2 generator = new ModelGenerator2();
+        ModelGenerator2 generator = new ModelGenerator2();
 
-      for (int ii = 0; ii < designFiles.Count; ii++)
-      {
+        for (int ii = 0; ii < designFiles.Count; ii++)
+        {
         if (String.IsNullOrEmpty(designFiles[ii]))
-        {
-          throw new ArgumentException("No design file specified.");
-        }
+          {
+            throw new ArgumentException("No design file specified.");
+          }
 
-        if (!new FileInfo(designFiles[ii]).Exists)
-        {
-          throw new ArgumentException("The design file does not exist: " + designFiles[ii]);
+          if (!new FileInfo(designFiles[ii]).Exists)
+          {
+            throw new ArgumentException("The design file does not exist: " + designFiles[ii]);
+          }
         }
-      }
 
       if (String.IsNullOrEmpty(identifierFile))
-      {
-        throw new ArgumentException("No identifier file specified.");
-      }
-
-      if (!new FileInfo(identifierFile).Exists)
-      {
-        if (!generateIds)
         {
-          throw new ArgumentException("The identifier file does not exist: " + identifierFile);
+          throw new ArgumentException("No identifier file specified.");
         }
 
-        File.Create(identifierFile).Close();
-      }
+        if (!new FileInfo(identifierFile).Exists)
+        {
+          if (!generateIds)
+          {
+            throw new ArgumentException("The identifier file does not exist: " + identifierFile);
+          }
 
-      generator.ValidateAndUpdateIds(
-          designFiles,
-          identifierFile,
-          startId,
-          specificationVersion,
-          useAllowSubtypes);
+          File.Create(identifierFile).Close();
+        }
+
+        generator.ValidateAndUpdateIds(
+            designFiles,
+            identifierFile,
+            startId,
+            specificationVersion,
+            useAllowSubtypes);
 
       if (!String.IsNullOrEmpty(stackRootDir))
-      {
-        if (!new DirectoryInfo(stackRootDir).Exists)
         {
-          throw new ArgumentException("The directory does not exist: " + stackRootDir);
-        }
+          if (!new DirectoryInfo(stackRootDir).Exists)
+          {
+            throw new ArgumentException("The directory does not exist: " + stackRootDir);
+          }
 
-        StackGenerator.GenerateDotNet(stackRootDir, specificationVersion);
-      }
+          StackGenerator.GenerateDotNet(stackRootDir, specificationVersion);
+        }
 
       if (!String.IsNullOrEmpty(ansicRootDir))
-      {
-        if (!new DirectoryInfo(ansicRootDir).Exists)
         {
-          throw new ArgumentException("The directory does not exist: " + ansicRootDir);
+          if (!new DirectoryInfo(ansicRootDir).Exists)
+          {
+            throw new ArgumentException("The directory does not exist: " + ansicRootDir);
+          }
+
+          StackGenerator.GenerateAnsiC(ansicRootDir, specificationVersion);
+          generator.GenerateIdentifiersAndNamesForAnsiC(ansicRootDir, excludeCategories);
         }
 
-        StackGenerator.GenerateAnsiC(ansicRootDir, specificationVersion);
-        generator.GenerateIdentifiersAndNamesForAnsiC(ansicRootDir, excludeCategories);
+      if (!String.IsNullOrEmpty(outputDir))
+        {
+          if (generateMultiFile)
+            generator.GenerateMultipleFiles(outputDir, useXmlInitializers, excludeCategories, includeDisplayNames);
+          else
+            generator.GenerateInternalSingleFile(outputDir, useXmlInitializers, excludeCategories, includeDisplayNames);
+        }
       }
 
-      if (!String.IsNullOrEmpty(outputDir))
-      {
-        if (generateMultiFile)
-          generator.GenerateMultipleFiles(outputDir, useXmlInitializers, excludeCategories, includeDisplayNames);
-        else
-          generator.GenerateInternalSingleFile(outputDir, useXmlInitializers, excludeCategories, includeDisplayNames);
-      }
     }
   }
 }
