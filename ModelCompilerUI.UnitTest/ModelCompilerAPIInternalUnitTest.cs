affa1dfa3b212ae92e3966d299f8e7afab88d41a
Mariusz <mpostol@cas.eu>
2021-10-12 23:55:30 +0200

    Move command line parser to UI #33
    = Working on UT.

--- ModelCompilerUI.UnitTest/ModelCompilerAPIInternalUnitTest.cs
+++ ModelCompilerUI.UnitTest/ModelCompilerAPIInternalUnitTest.cs
@@ -28,6 +28,7 @@ namespace OOI.ModelCompilerUI
       ModelCompilerAPIInternal instance = new ModelCompilerAPIInternal();
       List<string> commandLine = new List<string>()
       {
+        "exeName",
         "-d2", @".\Opc.Ua.ModelCompiler\Design.v104\StandardTypes.xml",
         "-version",  "v104",
         "-d2",  @".\ModelCompiler\Design.v104\UA Core Services.xml",
@@ -38,7 +39,16 @@ namespace OOI.ModelCompilerUI
       };
       instance.ProcessCommandLine(commandLine);
       Assert.AreEqual<string>(@".\Bin\nodesets\master\AnsiC\", instance.ansicRootDir);
+      Assert.AreEqual<int>(2, instance.designFiles.Count);
+      Assert.AreEqual<string>(@".\Opc.Ua.ModelCompiler\Design.v104\StandardTypes.xml", instance.designFiles[0]);
+      Assert.AreEqual<string>(@".\ModelCompiler\Design.v104\UA Core Services.xml", instance.designFiles[1]);
       Assert.AreEqual<string>("*.xml", instance.filePattern);
+      Assert.AreEqual<string>(@".\ModelCompiler\CSVs\StandardTypes.csv", instance.identifierFile);
+      Assert.IsFalse(instance.generateIds);
+      Assert.AreEqual<string>(@".\Bin\nodesets\master\Schema\", instance.outputDir);
+      Assert.AreEqual<string>(@".\Bin\nodesets\master\DotNet\", instance.stackRootDir);
+      Assert.AreEqual<string>("v104", instance.specificationVersion);
+      Assert.IsFalse(instance.updateHeaders);
     }
 
     [TestMethod]
@@ -47,12 +57,18 @@ namespace OOI.ModelCompilerUI
       ModelCompilerAPIInternal instance = new ModelCompilerAPIInternal();
       List<string> commandLine = new List<string>()
       {
+        "exeName",
         "-version", "v104",
         "-d2", @".\Opc.Ua.ModelCompiler\Design.v104\DemoModel.xml",
         "-cg", @".\ModelCompiler\CSVs\DemoModel.csv",
         "-o2", @".\Bin\nodesets\master\DemoModel\"
       };
       instance.ProcessCommandLine(commandLine);
+      Assert.IsTrue(instance.generateIds);
+      Assert.AreEqual<string>(@".\ModelCompiler\CSVs\DemoModel.csv", instance.identifierFile);
+      Assert.AreEqual<string>("v104", instance.specificationVersion);
+      Assert.AreEqual<string>(null, instance.stackRootDir);
+      Assert.IsFalse(instance.updateHeaders);
     }
 
     [TestMethod]
@@ -67,6 +83,9 @@ namespace OOI.ModelCompilerUI
         "-silent"
       };
       instance.ProcessCommandLine(commandLine);
+      Assert.AreEqual<string>(null, instance.stackRootDir);
+      Assert.IsFalse(instance.generateIds);
+      Assert.IsTrue(instance.updateHeaders);
     }
   }
 }
