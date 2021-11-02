740515366f140a8071121390dbb0f12cac8b14da
Mariusz <mpostol@cas.eu>
2021-11-02 22:36:31 +0100

    Move stack generation to the UT #35
    - Refactoring - separated stack generation and model building functionality to independent files.
    - stack generator is ready to be moved elsewhere.
    - UT :+1:

--- Opc.Ua.ModelCompiler/ModelCompilerAPI.cs
+++ Opc.Ua.ModelCompiler/ModelCompilerAPI.cs
@@ -40,16 +40,4 @@ namespace OOI.ModelCompiler
       }
     }
   }
-
-  public static class ModelCompiler
-  {
-    public static void BuildModel(string outputDir, IModelGeneratorGenerate generateParameters, IModelGeneratorValidate validateParameters)
-    {
-      if (string.IsNullOrEmpty(outputDir))
-        throw new ArgumentOutOfRangeException("OutputDir", "Parameters cannot be null or empty");
-      ModelGenerator2 Generator = new ModelGenerator2();
-      Generator.ValidateAndUpdateIds(validateParameters);
-      Generator.Generate(generateParameters, outputDir);
-    }
-  }
 }
