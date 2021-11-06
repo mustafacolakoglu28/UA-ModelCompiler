3265a2c59a1c0c33cc2e080bfb50b3a2ffff41a0
Mariusz <mpostol@cas.eu>
2021-11-06 23:07:34 +0100

    Create API browser #61
    - refactoring to remove from the API useless definition

--- Tests/DemoModel/DemoModel.DataTypes.cs
+++ Tests/DemoModel/DemoModel.DataTypes.cs
@@ -1060,7 +1060,7 @@ namespace DemoModel
 
         #if !NET_STANDARD
         /// <summary cref="ICloneable.Clone" />
-        public object Clone()
+        public override object Clone()
         {
             return (SampleUnion)this.MemberwiseClone();
         }
@@ -1571,7 +1571,7 @@ namespace DemoModel
 
         #if !NET_STANDARD
         /// <summary cref="ICloneable.Clone" />
-        public object Clone()
+        public override object Clone()
         {
             return (SampleUnionAllowSubtypes)this.MemberwiseClone();
         }
