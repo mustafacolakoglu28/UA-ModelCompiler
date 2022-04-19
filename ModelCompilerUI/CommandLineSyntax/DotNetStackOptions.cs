a3a158724aa0d525a84b0c28aff06ea51d4dd4b5
MPostol <mpostol@cas.eu>
2022-04-19 21:11:20 +0200

    CLI Syntax #67
    - tested GUI using DemoModel and ModelDesign
    - UT :+1:
    - fixes #67
    - status rc 3.0

--- ModelCompilerUI/CommandLineSyntax/DotNetStackOptions.cs
+++ ModelCompilerUI/CommandLineSyntax/DotNetStackOptions.cs
@@ -10,13 +10,20 @@ using CommandLine;
 namespace OOI.ModelCompilerUI.CommandLineSyntax
 {
   [Verb("stack", false, HelpText = "Generates code for the core model (not used for vendor defined models)")]
-  internal class DotNetStackOptions: OptionsBase
+  internal class DotNetStackOptions : OptionsBase
   {
     [Option('n', OptionsNames.DotNetStackPath, HelpText = dotnetHelp, Required = false)]
     public string DotNetStackPath { get; set; }
 
     [Option('a', OptionsNames.AnsiCStackPath, HelpText = ansicHelp, Required = false)]
     public string AnsiCStackPath { get; set; }
+
+    public override string ToString()
+    {
+      return Parser.Default.FormatCommandLine<DotNetStackOptions>(this);
+    }
+
+    #region private
 
     private const string ansicHelp = "Generates the ANSI C stack code for the core model (not used for vendor defined models). The path to use when generating ANSI C stack code.";
     private const string dotnetHelp = "Generates the .NET stack code for the core model (not used for vendor defined models). The path to use when generating .NET stack code.";
@@ -26,5 +33,7 @@ namespace OOI.ModelCompilerUI.CommandLineSyntax
       public const string AnsiCStackPath = "ansic";
       public const string DotNetStackPath = "dotnet";
     }
+
+    #endregion private
   }
 }
