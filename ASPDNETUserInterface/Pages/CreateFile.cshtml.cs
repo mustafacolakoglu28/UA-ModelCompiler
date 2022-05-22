f7e9bd6a7e308c7177add4aa679fcf679232b9dc
mustafacolakoglu28 <mstfclkglu@gmail.com>
2022-05-22 21:36:33 +0200

    Added File Update

---
+++ ASPDNETUserInterface/Pages/CreateFile.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace OOI.ModelCompiler.ASPDNETUserInterface.Pages
+{
+    public class CreateFileModel : PageModel
+    {
+        private IHostingEnvironment Environment;
+        public CreateFileModel(IHostingEnvironment _environment)
+        {
+            Environment = _environment;
+        }
+
+        
+
+        public void OnPostUpload(List<IFormFile> postedFiles)
+        {
+            
+
+            string path = Path.Combine(this.Environment.WebRootPath, "Temp/InputFiles");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            foreach (IFormFile postedFile in postedFiles)
+            {
+                string fileName = Path.GetFileName(postedFile.FileName);
+                using (FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+            }
+        }
+    }
+}
